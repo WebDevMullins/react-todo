324b904f1ded602750e5b0fbfe9c6cdbe3f630f7
Brandon Mullins <webdevmullins@gmail.com>
2024-10-19 10:20:00 -0500

    initial commit, add vite, tailwind, shadcn

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-3xl'>Todo App</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
